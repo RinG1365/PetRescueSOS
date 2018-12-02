eeac08a6f5204d865c61c6476b06debf9b20f5d1
RinG1365 <44746272+RinG1365@users.noreply.github.com>
2018-12-03 05:10:18 +0800

    Update config.php

--- webService/config.php
+++ webService/config.php
@@ -1,1 +1,11 @@
+<?php
+$hostAddr = "127.0.0.1";
+$dbName = "petRescue";
+$dbUser = "root";
+$dbPwd = "root";
+$dbPort = 3306;
+
+$dbPDO = new PDO("mysql:host=$hostAddr;dbname=$dbName",$dbUser,$dbPwd);
+
+?>
 
