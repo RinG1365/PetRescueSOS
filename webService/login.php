5b27388b01210d142612f892165a3d5601c0c8ff
RinG1365 <44746272+RinG1365@users.noreply.github.com>
2018-12-03 05:10:47 +0800

    Create login.php

---
+++ webService/login.php
@@ -0,0 +1,90 @@
+<?php
+
+
+require_once("config.php");
+
+
+if(isset($_POST))
+{
+  $varFN=$_REQUEST["selectFn"];
+
+  if ($varFN =="login")
+  {
+    $username = $_REQUEST["username"];
+    $pass = $_REQUEST["password"];
+
+    if($username == "admin")
+    {
+          $adminLogin = "SELECT * FROM ADMIN WHERE ID = :login AND PSS = :pass";
+          $stmt = $dbPDO->prepare($adminLogin);
+          $stmt->execute(array(':login'=>$username,':pass'=>$pass));
+
+          $count = $stmt->rowCount();
+          if($count != 0)
+          {
+            $level = array('level'=>1);
+          }
+          else
+          {
+            $level = array('level'=>-1);
+          }
+    }
+
+    else
+    {
+      $userLogin = "SELECT * FROM USERS WHERE USERNAME = :login AND PSS = :pass";
+      $stmt = $dbPDO->prepare($userLogin);
+      $stmt ->execute(array(':login'=>$username,':pass'=>$pass));
+
+      $count = $stmt->rowCount();
+      if($count != 0)
+      {
+        $level = array('level'=>2);
+      }
+      else
+      {
+        $level = array('level'=>-1);
+      }
+    }
+
+    echo json_encode($level);
+  }
+
+  if($varFN == "register")
+  {
+    $username = $_REQUEST["username"];
+    $pass = $_REQUEST["password"];
+    $gender = $_REQUEST["gender"];
+    $mobile = $_REQUEST["mobile"];
+
+    $checkUser = "SELECT * FROM USERS WHERE USERNAME = :login";
+    $stmt = $dbPDO->prepare($checkUser);
+    $stmt ->execute(array(':login'=>$username));
+
+    $count = $stmt->rowCount();
+
+    if($count == 0)
+    {
+      $addUser = "INSERT INTO USERS (USERNAME, PSS, GENDER, PHONENO) VALUES(:login, :pass, :gen, :mn)";
+      $stmt = $dbPDO->prepare($addUser);
+      $stmt -> execute(array(':login'=>$username,':pass'=>$pass,':gen'=>$gender,':mn'=>$mobile));
+
+
+      $success = array('success'=>1);
+
+    }
+    else
+    {
+      $success = array('success'=>-1);
+    }
+
+
+    echo json_encode($success);
+  }
+
+
+
+}
+
+
+ ?>
