0c7c8d0b40cd96c54ef7b119942cdbb1cf746fb3
RinG1365 <44746272+RinG1365@users.noreply.github.com>
2018-12-03 05:14:43 +0800

    Create History.java

---
+++ model/History.java
@@ -0,0 +1,90 @@
+package model;
+
+public class History {
+	
+	String sosId;
+	String sosDate;
+	String sosTime;
+	String ptype;
+	String pquan;
+	String username;
+	String rid;
+	String sid;
+	String status;
+	
+	public History() {}
+
+	public String getSosId() {
+		return sosId;
+	}
+
+	public void setSosId(String sosId) {
+		this.sosId = sosId;
+	}
+
+	public String getSosDate() {
+		return sosDate;
+	}
+
+	public void setSosDate(String sosDate) {
+		this.sosDate = sosDate;
+	}
+
+	public String getSosTime() {
+		return sosTime;
+	}
+
+	public void setSosTime(String sosTime) {
+		this.sosTime = sosTime;
+	}
+
+	public String getPtype() {
+		return ptype;
+	}
+
+	public void setPtype(String ptype) {
+		this.ptype = ptype;
+	}
+
+	public String getPquan() {
+		return pquan;
+	}
+
+	public void setPquan(String pquan) {
+		this.pquan = pquan;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRid() {
+		return rid;
+	}
+
+	public void setRid(String rid) {
+		this.rid = rid;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+	public String getSid() {
+		return sid;
+	}
+
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+
+}
